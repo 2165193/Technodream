284c8b5191d36f51a57cc32d28f9271c38dae3ae
2165193 <markkennethmarzan@gmail.com>
2018-06-29 17:09:16 +0800

    latest

---
+++ homeinspectorstrose.com/app/view/home.php
@@ -0,0 +1,104 @@
+<div id="content">
+	<div class="row">
+		<div class="contTop">
+			<div class="imgBox">
+				<img src="public/images/content/contentIcon.png" alt="">
+				<h1>ABOUT US</h1>
+			</div>
+			<p>There’s no question that buying a new house is one of the biggest financial decisions you will make in your life. Before doing this, you’ll want to make sure that the property is worth what the owner is charging. At Morgan Home Inspection, LLC , our home inspector will help you figure this out with any of our home inspection services. Our building inspector wants to help you to get the best price available, without spending more money than you need to.</p>
+		</div>
+		<div class="contBot">
+			<div class="contLeft fl">
+				<div class="imgCont">
+					<img src="public/images/content/contentPerson.png" alt="">		
+				</div>
+			</div>
+			<div class="contRight fr">
+				<img src="public/images/content/name.png" alt="" class="fl">
+				<div class="clearfix"></div>
+				<div class="innerContTop">
+					<p>There’s no question that buying a new house is one of the biggest financial decisions you will make in your life. Before doing this, you’ll want to make sure that the property is worth what the owner is charging. At Morgan Home Inspection, LLC , our home inspector will help you figure this out with any of our home inspection services. Our building inspector wants to help you to get the best price available, without spending more money than you need to.</p>
+					<p>Here at Morgan Home Inspection, LLC, we sustain our company on reliability, great communication, integrity, and quality work. We strongly believe in giving our absolute best in all of our projects. We are continually studying our trade and will always keep you educated on your particular task or project.</p>
+					<p>Given what’s at stake, you have every reason to get home and roof inspection. The best way for residents of St. Rose, LA to do this is to contact us at Morgan Home Inspection, LLC and don’t forget to ask us about out sewer drain inspection.</p>
+				</div>
+				<div class="innerContBot">
+					<p class="imgContBot">
+						<img src="public/images/content/contentImg1.png" alt="">
+						<img src="public/images/content/contentImg2.png" alt="">
+						<img src="public/images/content/contentImg3.png" alt="">
+						<a href="_blank">LEARN MORE</a>
+					</p>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
+<div id="reviews">
+	<div class="row">	
+		<div class="containerLeft fl">	
+			<h4>The greatest tragedy <span>is indifference.</span></h4>
+			<div class="innerCont">	
+				<a href="contact#content">FREE ESTIMATE</a>
+			</div>
+			<div class="clearfix">	</div>
+		</div>
+		<div class="containerRight fr">		
+			<div class="contTop">	
+				<img src="public/images/content/contentIcon.png" alt="">	
+				<h3>REVIEWS</h3>
+				<p>Jimi is very courteous. He called the night before our appointment to confirm and he arrived the next morning on time (actually 15 minutes early). He was very efficient and clearly explained things to us. He found the broken drain pipe quickly with his camera and recorded it for us. I will definitely use this service in the future if needed and will recommend him highly to others.</p>
+				<span>-Kathryn L.</span>
+			</div>
+			<div class="contBot">	
+				<p>
+					<img src="public/images/common/reviewsImg1.png" alt="">
+					<img src="public/images/common/reviewsImg2.png" alt="">
+					<img src="public/images/common/reviewsImg3.png" alt="">
+					<img src="public/images/common/reviewsImg4.png" alt="">
+				</p>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>			
+</div>
+<div id="services">	
+	<div class="row">	
+		<img src="public/images/content/contentIcon.png" alt="">
+		<h3>SERVICES</h3>
+		<div class="container">
+			<dl>	
+				<dt><img src="public/images/common/servicesImg1.png" alt=""></dt>
+				<dd>MOLD <span>INSPECTION</span></dd>
+			</dl>
+			<dl>	
+				<dt><img src="public/images/common/servicesImg2.png" alt=""></dt>
+				<dd>COMMERCIAL <span>BUILDING INSPECTION</span></dd>
+			</dl>
+			<dl>	
+				<dt><img src="public/images/common/servicesImg3.png" alt=""></dt>
+				<dd>HOME <span>INSPECTION</span></dd>
+			</dl>
+		</div>
+		<a href="_blank">LEARN MORE</a>
+	</div>	
+</div>	
+<div id="map">
+	<div class="row">
+		<div class="container fl">
+			<span>SERVICE AREAS</span>
+			<ul>
+				<li><span>New Orleans</span></li>
+				<li><span>Baton Rouge</span></li>
+				<li><span>Lafayette</span></li>
+				<li><span>Metairie</span></li>
+				<li><span>Kenner</span></li>
+				<li><span>Slidell</span></li>
+				<li><span>Houma</span></li>
+				<li><span>Hammond</span></li>
+				<li><span>Denham Springs</span></li>
+			</ul>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
---
+++ homeinspectorstrose.com/app/view/template/header.php
@@ -0,0 +1,53 @@
+<?php $this->suspensionRedirect($view); ?>
+<!DOCTYPE html>
+<html lang="en" <?php $this->helpers->htmlClasses(); ?>>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+
+	<?php $this->helpers->seo($view); ?>
+	<link rel="icon" href="public/images/favicon.png" type="image/x-icon">
+	<!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+	<link href="<?php echo URL; ?>public/styles/style.css" rel="stylesheet">
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+	<link rel="stylesheet" href="<?php echo URL; ?>public/fancybox/source/jquery.fancybox.css" media="screen" />
+	<?php $this->helpers->analytics(); ?>
+</head>
+
+<body <?php $this->helpers->bodyClasses($view); ?>>
+<?php $this->checkSuspensionHeader(); ?>
+	<header>
+		<div id="header">
+			<div class="row">
+				<div class="logo fl">
+					<img src="public/images/common/hdLogo.png" alt="">
+				</div>
+				<div class="container fr">
+					<a href="<?php $this->info("fb_link")?>"><img src="public/images/common/hdImg1.png" alt="" class="fb"></a>
+					<a href="<?php $this->info("yp_link")?>"><img src="public/images/common/hdImg2.png" alt="" class="yelp"></a>
+					<nav>
+						<a href="#" id="pull"><strong>MENU</strong></a>
+						<ul>
+							<li <?php $this->helpers->isActiveMenu("home"); ?>><a href="<?php echo URL ?>">Home</a></li>
+							<li <?php $this->helpers->isActiveMenu("about"); ?>><a href="<?php echo URL ?>about#content">About Us</a></li>
+							<li <?php $this->helpers->isActiveMenu("gallery"); ?>><a href="<?php echo URL ?>gallery#content">Gallery</a></li>
+							<li <?php $this->helpers->isActiveMenu("services"); ?>><a href="<?php echo URL ?>services#content">Services</a></li>
+							<li <?php $this->helpers->isActiveMenu("contact"); ?>><a href="<?php echo URL ?>contact#content">Contact Us</a></li>
+							<li <?php $this->helpers->isActiveMenu("privacy-policy"); ?>><a href="<?php echo URL ?>privacy-policy#content">Privacy Policy</a></li>
+						</ul>
+					</nav>
+				</div>
+				<div class="clearfix"></div>
+			</div>		
+		</div>
+	</header>
+	<div id="banner">
+		<div class="row">
+			<h2>When you care enough <span>to send the very best.</span></h2>
+			<div class="border">
+				<a href="contact#content">FREE ESTIMATE</a>
+			</div>
+		</div>
+	</div>
