e6d2034a152a8f6798362209b9d62b76b2a99801
2165193 <markkennethmarzan@gmail.com>
2018-07-09 09:22:20 +0800

    revised

---
+++ homeinspectorstrose.com_rev/app/view/home.php
@@ -0,0 +1,105 @@
+<div id="content">
+	<div class="row">
+		<div class="contTop">
+			<div class="imgBox">
+				<img src="public/images/content/contentIcon.png" alt="">
+				<h1>ABOUT THE OWNER</h1>
+			</div>
+			<p>Here at Morgan Home Inspection, LLC, we sustain our company on reliability, great communication, integrity, and quality work. We strongly believe in giving our absolute best in all of our projects. We are continually studying our trade and will always keep you educated on your particular task or project.</p>
+		</div>
+		<div class="contBot">
+			<div class="contLeft fl">
+				<div class="imgCont">
+					<img src="public/images/content/contentPerson.png" alt="">		
+				</div>
+			</div>
+			<div class="contRight fr">
+				<img src="public/images/content/name.png" alt="" class="fl">
+				<div class="clearfix"></div>
+				<div class="innerContTop">
+					<p>I chose to work in this industry because Choosing the right home inspector can be difficult. Unlike most professionals, you probably will not get to meet me until after you hire me. Furthermore, different inspectors have varying qualifications, equipment, experience, reporting methods, and, yes, different pricing. One thing for sure is that a home inspection requires work -- a lot of work. Ultimately, a thorough inspection depends heavily on the individual inspector’s own effort. If you honor me by permitting me to inspect your new home, I guarantee that I will give you my very best effort. This, I promise you..</p>
+					<p>We started in this industry in 1985.</p>
+					<p>We describe our business as Multi-generational</p>
+                    <p>Given what’s at stake, you have every reason to get home and roof inspection. The best way for residents of New Orleans, LA to do this is to contact us at Morgan Home Inspection, LLC and don’t forget to ask us about out sewer drain inspection.</p>
+				</div>
+				<div class="innerContBot">
+					<p class="imgContBot">
+						<img src="public/images/content/contentImg1.png" alt="">
+						<img src="public/images/content/contentImg2.png" alt="">
+						<img src="public/images/content/contentImg3.png" alt="">
+						<a href="about#content">LEARN MORE</a>
+					</p>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</div>
+<div id="reviews">
+	<div class="row">	
+		<div class="containerLeft fl">	
+			<h4>The greatest tragedy <span>is indifference.</span></h4>
+			<div class="innerCont">	
+				<a href="contact#content">FREE ESTIMATE</a>
+			</div>
+			<div class="clearfix">	</div>
+		</div>
+		<div class="containerRight fr">		
+			<div class="contTop">	
+				<img src="public/images/content/contentIcon.png" alt="">	
+				<h3>REVIEWS</h3>
+				<p>Jimi is very courteous. He called the night before our appointment to confirm and he arrived the next morning on time (actually 15 minutes early). He was very efficient and clearly explained things to us. He found the broken drain pipe quickly with his camera and recorded it for us. I will definitely use this service in the future if needed and will recommend him highly to others.</p>
+				<span>-Kathryn L.</span>
+			</div>
+			<div class="contBot">	
+				<p>
+					<img src="public/images/common/reviewsImg1.png" alt="">
+					<img src="public/images/common/reviewsImg2.png" alt="">
+					<img src="public/images/common/reviewsImg3.png" alt="">
+					<img src="public/images/common/reviewsImg4.png" alt="">
+				</p>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+	</div>			
+</div>
+<div id="services">	
+	<div class="row">	
+		<img src="public/images/content/contentIcon.png" alt="">
+		<h3>SERVICES</h3>
+		<div class="container">
+			<dl>	
+				<dt><img src="public/images/common/servicesImg1.png" alt=""></dt>
+				<a href="services#content"><dd>MOLD <span>INSPECTION</span></dd></a>
+			</dl>
+			<dl>	
+				<dt><img src="public/images/common/servicesImg2.png" alt=""></dt>
+				<a href="services#content"><dd>COMMERCIAL <span>BUILDING INSPECTION</span></dd></a>
+			</dl>
+			<dl>	
+				<dt><img src="public/images/common/servicesImg3.png" alt=""></dt>
+				<a href="services#content"><dd>HOME <span>INSPECTION</span></dd></a>
+			</dl>
+		</div>
+		<a href="services#content" class="learn">LEARN MORE</a>
+	</div>	
+</div>	
+<div id="map">
+	<div class="row">
+		<div class="container fl">
+			<span>SERVICE AREAS</span>
+			<ul>
+				<li><span>New Orleans</span></li>
+				<li><span>Baton Rouge</span></li>
+				<li><span>Lafayette</span></li>
+				<li><span>Metairie</span></li>
+				<li><span>Kenner</span></li>
+				<li><span>Slidell</span></li>
+				<li><span>Houma</span></li>
+				<li><span>Hammond</span></li>
+				<li><span>Denham Springs</span></li>
+			</ul>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
---
+++ homeinspectorstrose.com_rev/app/view/template/footer.php
@@ -0,0 +1,147 @@
+<footer>
+	<div class="row">
+		<div class="footerTop">
+			<div class="imgBox">
+				<img src="public/images/common/footerPhoneIcon.png" alt="">
+				<h3>CONTACT US</h3>
+			</div>
+			<div class="container">
+				<div class="col col1">
+					<span class="fl">Call Us Today</span>
+					<div class="clearfix"></div>
+					<div class="innerCont">
+						<p>Given what’s at stake, you have every reason to get home and roof inspection. The best way for residents of St. Rose, LA to do this is to contact us at Morgan Home Inspection, LLC and don’t forget to ask us about out sewer drain inspection.</p>
+						<a href="contact#content">LEARN MORE</a>
+					</div>
+				</div>
+				<div class="col col2">
+					<div class="container">
+						<span class="fl">Navigation</span>
+						<div class="clearfix"></div>
+						<div class="innerCont">
+							<ul>
+								<li <?php $this->helpers->isActiveMenu("home"); ?>><a href="<?php echo URL ?>">Home</a></li>
+								
+								<li <?php $this->helpers->isActiveMenu("services"); ?>><a href="<?php echo URL ?>services#content">Services</a></li>
+								<li <?php $this->helpers->isActiveMenu("gallery"); ?>><a href="<?php echo URL ?>gallery#content">Gallery</a></li>
+								<li <?php $this->helpers->isActiveMenu("privacy-policy"); ?>><a href="<?php echo URL ?>testimonials#content">Testimonials</a></li>
+								<li <?php $this->helpers->isActiveMenu("contact"); ?>><a href="<?php echo URL ?>contact#content">Contact Us</a></li>
+                                <li <?php $this->helpers->isActiveMenu("privacy-policy"); ?>><a href="<?php echo URL ?>privacy-policy#content">Privacy Policy</a></li>
+							</ul>
+							<div class="clearfix"></div>
+							<p>
+								<a href="<?php $this->info("fb_link")?>"><img src="public/images/common/hdImg1.png" alt=""></a>
+								<a href="<?php $this->info("yp_link")?>"><img src="public/images/common/hdImg2.png" alt=""></a>
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col col3">
+					<div class="container">
+						<span class="fl">Informations</span>
+						<div class="clearfix"></div>
+						<div class="innerCont">
+							<div class="innerCont">
+								<p><img src="public/images/common/footerImg1.png" class="first" alt=""><?php $this->info("address");?></p>
+								<p><img src="public/images/common/footerImg2.png" alt=""><?php $this->info(["email","mailto"]);?></p>
+								<p><img src="public/images/common/footerImg3.png" alt=""><?php $this->info(["phone","tel"]);?></p>
+								<img src="public/images/common/infoImg.png" alt="" class="last">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<p class="copy">
+			© <?php echo date("Y"); ?>. <?php $this->info("company_name"); ?> All Rights Reserved. 
+			<?php if( $this->siteInfo['policy_link'] ): ?>
+				<a href="<?php $this->info("policy_link"); ?>">Privacy Policy</a>. 
+			<?php endif ?>
+		</p>
+		<p class="silver"><img src="public/images/scnt.png" alt="" class="company-logo" /><a href="https://silverconnectwebdesign.com/website-development" rel="external" target="_blank"> Web Design</a> Done by <a href="https://silverconnectwebdesign.com" rel="external" target="_blank">Silver Connect Web Design</a></p>
+	</div>
+</footer>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="<?php echo URL; ?>public/scripts/sendform.js" data-view="<?php echo $view; ?>" id="sendform"></script>
+<!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>  -->
+<script src="<?php echo URL; ?>public/scripts/responsive-menu.js"></script>
+<script src="https://unpkg.com/sweetalert2@7.20.10/dist/sweetalert2.all.js"></script>
+
+<?php if( $this->siteInfo['cookie'] ): ?>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+	<script>
+	window.addEventListener("load", function(){
+	window.cookieconsent.initialise({
+	  "palette": {
+	    "popup": {
+	      "background": "#000"
+	    },
+	    "button": {
+	      "background": "#3085d6"
+	    }
+	  }
+	})});
+	</script>
+<?php endif ?>
+
+<?php if(in_array($view,["home","contact"])): ?>
+	<script src='//www.google.com/recaptcha/api.js?onload=captchaCallBack&render=explicit' async defer></script>
+	<script>
+		var captchaCallBack = function() {
+			$('.g-recaptcha').each(function(index, el) {
+				grecaptcha.render(el, {'sitekey' : '<?php $this->info("site_key");?>'});
+			});
+		};
+
+		$('.consentBox').click(function () {
+		    if ($(this).is(':checked')) {
+		    	if($('.termsBox').length){
+		    		if($('.termsBox').is(':checked')){
+		        		$('.ctcBtn').removeAttr('disabled');
+		        	}
+		    	}else{
+		        	$('.ctcBtn').removeAttr('disabled');
+		    	}
+		    } else {
+		        $('.ctcBtn').attr('disabled', true);
+		    }
+		});
+
+		$('.termsBox').click(function () {
+		    if ($(this).is(':checked')) {
+	    		if($('.consentBox').is(':checked')){
+	        		$('.ctcBtn').removeAttr('disabled');
+	        	}
+		    } else {
+		        $('.ctcBtn').attr('disabled', true);
+		    }
+		});
+
+	</script>
+
+<?php endif; ?>
+
+
+<?php if ($view == "gallery"): ?>
+	<script type="text/javascript" src="<?php echo URL; ?>public/fancybox/source/jquery.fancybox.js?v=2.1.5"></script>
+	<script type="text/javascript" src="<?php echo URL; ?>public/scripts/jquery.pajinate.js"></script>
+	<script>
+		$('#gall1').pajinate({ num_page_links_to_display : 5, items_per_page : 10 });
+		$('.fancy').fancybox({
+			helpers: {
+				title : {
+					type : 'over'
+				}
+			}
+		});
+	</script>
+<?php endif; ?>
+
+<?php $this->checkSuspensionFooter(); ?>
+</body>
+</html>
+
+
