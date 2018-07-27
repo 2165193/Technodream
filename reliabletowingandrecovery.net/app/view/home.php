4356407da217a405f0df478bf321ae7cb0b13287
2165193 <markkennethmarzan@gmail.com>
2018-07-27 15:48:03 +0800

    done

---
+++ reliabletowingandrecovery.net/app/view/home.php
@@ -0,0 +1,212 @@
+<div id="content">
+	<div id="content-top">
+		<div class="row">
+			<span class="welcome">WELCOME TO</span>
+			<h1>RELIABLE TOWING AND RECOVERY</h1>
+			<span class="sub">High Quality Services at Great Prices!</span>
+			<div class="container">
+				<p><strong>Reliable Towing and Recovery</strong>, is locally owned, operated and has been providing honest and reliable <strong>Towing Services</strong> in <strong>Canutillo, Texas</strong> - as well as throughout El Paso County and the surrounding areas.
+				<p><strong>Reliable Towing and Recovery</strong> maintains a consistently higher level of responsiveness and customer service, providing customers with the best quality workmanship at competitive prices.</p>
+				<p>We look forward to speaking with you and learning more about how we can serve you. Please call us today at <strong><?php $this->info(["phone","tel"]);?></strong></p>
+			</div>
+		</div>
+	</div>
+	<div id="content-bot">
+		<div class="row">
+			<div class="link flex space-btw">
+				<div class="box">Why we stand out from the rest</div>
+				<div class="box">Our Reputation</div>
+			</div>
+			<div class="container flex space-btw">
+				<ul>
+					<li>We truly care about our customers and give personal attention to each one of them.</li>
+					<li>Integrity and honesty from top to bottom.</li>
+					<li>Not compromising on less than the best customer service and responsiveness time.</li>
+					<li>Affordable and fair prices.</li>
+				</ul>
+				<ul>
+					<li>We earn our reputation on each and every call.</li>
+					<li>Serving you is our desire; pleasing you is our commitment.</li>
+				</ul>
+			</div>
+			<p>Thank you for visiting our website, to learn more about us navigate to other pages. The services we provide are automobile towing, rv towing, medium towing services, light towing, long distance towing and more. We do business in El Paso County and surrounding areas.</p>
+		</div>
+	</div>
+</div>
+<div id="services">
+	<div class="row">
+		<div class="cols col-1">
+			<div class="inner">
+				<h2>OUR SERVICES</h2>
+				<ul>
+					<li>Automobile Towing</li>
+					<li>Rv Towing</li>
+					<li>Flat Tire Change</li>
+					<li>Jumpstart & Dead Battery Services</li>
+					<li>Medium Towing Services</li>
+					<li>Gas Filling</li>
+					<li>Truck Towing</li>
+					<li>Light Towing</li>
+					<li>Accident Removal</li>
+					<li>Long Distance Towing</li>
+				</ul>
+			</div>
+		</div>
+		<div class="cols col-2">
+			<div class="inner">
+				<p>Individuals and businesses in a variety of industries can count on Reliable Towing and Recovery to provide a wide range of services related to Towing and Roadside Assistance Services. At Reliable Towing and Recovery, we strive to meet each client’s specific needs as quickly as possible, and even helping them with planning for possible future situations.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="gallery">
+	<div class="row">
+		<dl>
+			<dt><img src="public/images/content/galleryImg1.jpg" alt=""></dt>
+			<dd>
+				<h4>AUTOMOBILE TOWING</h4>
+			</dd>
+		</dl>
+		<dl>
+			<dt><img src="public/images/content/galleryImg2.jpg" alt=""></dt>
+			<dd>
+				<h4>TRUCK TOWING</h4>
+			</dd>
+		</dl>
+		<dl>
+			<dt><img src="public/images/content/galleryImg3.jpg" alt=""></dt>
+			<dd>
+				<h4>RV TOWING</h4>
+			</dd>
+		</dl>
+		<dl>
+			<dt><img src="public/images/content/galleryImg4.jpg" alt=""></dt>
+			<dd>
+				<h4>LIGHT TOWING</h4>
+			</dd>
+		</dl>
+	</div>
+</div>
+<div id="testimonials">
+	<div class="row">
+		<h2>TESTIMONIALS</h2>
+		<div class="container">
+			<div class="cols col-1">
+				<div class="author">
+					<img src="public/images/content/testiImg1.png" alt="">
+					<span>Chanell Bazemore</span>
+				</div>
+				<p>Not only is hector reliable and on time, but he’s very professional and straight forward. Didn’t try to overcharge me like most 24 hr towing companies would do. Eased my frustrations, I’d highly recommend this company. Quality service, easy on my wallet!!</p>
+			</div>
+			<div class="cols col-2">
+				<div class="author">
+					<img src="public/images/content/testiImg2.png" alt="">
+					<span>Hector Gonzalez</span>
+				</div>
+				<p>Great experience, great job and customer service!</p>
+			</div>
+			<div class="cols col-3">
+				<div class="author">
+					<img src="public/images/content/testiImg3.png" alt="">
+					<span>MaryAnn M.</span>
+				</div>
+				<p>Hector came out to the middle of the desert, at Kilbourne’s Hole. AAA, 911 couldn’t find us, Hector was sharp & did the job! Worked for over 2 hours to get us off a deep sandy slope! He didn’t have a 4 wheel drive tow truck, but he helped us with his 4x4 pick up. Not an easy job, but he had such good humor & professionalism, (and brought us more water!), it was a boost we needed in an exhausting day!</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div id="about">
+	<div class="row">
+		<div class="container fr">
+			<h2>ABOUT US</h2>
+			<p>Providing outstanding service to our clients comes from Reliable Towing and Recovery’s dedication to the three underlying principles of professionalism, responsiveness and quality. You can expect to save both time and money with Reliable Towing and Recovery.</p>
+		</div>
+		<div class="clearfix"></div>
+		<div class="imgBox">
+			<a href="<?php $this->info("yp_link"); ?>" target="_blank"><img src="public/images/content/aboutImg1.png" alt=""></a>
+		</div>
+	</div>
+</div>
+<div id="contacts">
+	<div class="row">
+		<div class="cols col-1">
+			<h4>Contact Us</h4>
+			<p><span><?php $this->info("address");?></span></p>
+			<p class="phone">PHONE: <span><?php $this->info(["phone","tel"]);?></span></p>
+			<p>EMAIL: <span><?php $this->info(["email","mailto"]);?></span></p>
+		</div>
+		<div class="cols col-2">
+			<h4>Follow Us</h4>
+			<p>Read our latest news on any of these social networks!</p>
+			<div class="imgBox">
+				<img src="public/images/content/followImg1.png" alt="">
+				<img src="public/images/content/followImg2.png" alt="">
+				<img src="public/images/content/followImg3.png" alt="">
+				<img src="public/images/content/followImg4.png" alt="">
+				<img src="public/images/content/followImg5.png" alt="">
+			</div>				
+		</div>
+		<div class="cols col-3">
+			<h4>Working Hours</h4>
+			<p>Monday - Sunday 24 Hours</p>
+			<div class="imgBox">
+				<img src="public/images/content/workImg1.png" alt="">
+				<img src="public/images/content/workImg2.png" alt="">
+				<img src="public/images/content/workImg3.png" alt="">
+				<img src="public/images/content/workImg4.png" alt="">
+				<img src="public/images/content/workImg5.png" alt="">
+			</div>
+		</div>
+	</div>
+</div>
+<div id="contactForm">
+	<div class="row">
+		<h2>Get In Touch</h2>
+		<form action="sendContactForm" method="post"  class="sends-email ctc-form fr" >
+			<div class="rows row-1">
+				<div class="cont">
+					<span class="ctc-hide">First Name</span>
+					<span class="ctc-hide">Email Address</span>
+				</div>
+				<label>
+					<input type="text" name="fname" placeholder="">
+				</label>
+				<label>
+					<input type="text" name="email" placeholder="">
+				</label>
+			</div>
+			<div class="rows row-2">
+				<div class="cont">
+					<span class="ctc-hide">Last Name</span>
+					<span class="ctc-hide">Subject</span>
+				</div>
+				<label>
+					<input type="text" name="lName" placeholder="">
+				</label>
+				<label>
+					<input type="text" name="subject" placeholder="">
+				</label>
+			</div>
+			<div class="rows row-3">
+				<div class="cont">
+					<span class="ctc-hide">Message</span>
+				</div>
+				<label>
+					<textarea name="message" cols="30" rows="10" placeholder=""></textarea>
+				</label>
+			</div>
+			<label><span class="ctc-hide"></span></label>
+			<div class="g-recaptcha"></div>
+			<label class="ads">
+				<input type="checkbox" name="consent" class="consentBox">I hereby consent to having this website store my submitted information so that they can respond to my inquiry.
+			</label><br>
+			<?php if( $this->siteInfo['policy_link'] ): ?>
+			<label class="ads">
+				<input type="checkbox" name="termsConditions" class="termsBox"/> I hereby confirm that I have read and understood this website's <a href="<?php $this->info("policy_link"); ?>" target="_blank">Privacy Policy.</a>
+			</label>
+			<?php endif ?>
+				<button type="submit" class="ctcBtn" disabled>Send Message</button>
+		</form>
+		<div class="clearfix"></div>
+	</div>
+</div>
